62d3a98e009f513e070c993638ffdddbe859cfe3
EzequielOS <zeque2020@gmail.com>
2022-07-05 01:06:07 -0300

    Organiza back end e torna funcionaÃl

--- imbMary/src/main/java/br/edu/iftm/imbMary/controller/VisitorController.java
+++ imbMary/src/main/java/br/edu/iftm/imbMary/controller/VisitorController.java
@@ -2,61 +2,16 @@ package br.edu.iftm.imbMary.controller;
 
 import br.edu.iftm.imbMary.domain.Visitor;
 import br.edu.iftm.imbMary.repository.VisitorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
-public class VisitorController {
+public class VisitorController extends GenericController<Visitor> {
 
-    @Autowired
-    VisitorRepository visitorRepository;
-
-    @GetMapping("/visitor")
-    public ResponseEntity<List<Visitor>> allVisitors(){
-        return ResponseEntity.status(HttpStatus.OK).body(visitorRepository.findAll());
-    }
-
-    @GetMapping("/visitor/{id}")
-    public ResponseEntity<Visitor> getVisitor(@PathVariable("id") Long id){
-        return ResponseEntity.status(HttpStatus.OK).body(visitorRepository.findById(id).orElseThrow(RuntimeException::new));
-    }
-
-    @PostMapping("/visitor")
-    public ResponseEntity<Visitor> newVisitor(@RequestBody @Valid Visitor newVisitor){
-        return ResponseEntity.status(HttpStatus.OK).body(visitorRepository.save(newVisitor));
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> validaCamposObrigatorios(MethodArgumentNotValidException ex){
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
-
-    @PutMapping("/visitor/{id}")
-    public ResponseEntity<Visitor> updateVisitor(@PathVariable("id") Long id, @RequestBody Visitor visitor){
-        visitor.setId(id);
-        return ResponseEntity.status(HttpStatus.OK).body(visitorRepository.save(visitor));
-    }
-
-    public ResponseEntity<Visitor> deleteVisitor(@PathVariable("id") Long id){
-        visitorRepository.deleteById(id);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public VisitorController(VisitorRepository visitorRepository){
+        super(visitorRepository);
     }
 }
--- imbMary/src/test/java/br/edu/iftm/imbMary/ImbMaryApplicationTests.java
+++ imbMary/src/test/java/br/edu/iftm/imbMary/ImbMaryApplicationTests.java
@@ -1,6 +1,5 @@
 package br.edu.iftm.imbMary;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
